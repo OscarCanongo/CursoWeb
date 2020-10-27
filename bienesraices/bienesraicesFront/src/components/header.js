b01738e2f57ed61e802905bb37409c771d29b70c
OscarCanongo <a01730443@itesm.mx>
2020-10-27 15:47:36 -0600

    CSS al Header

---
+++ bienesraices/bienesraicesFront/src/components/header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Navegacion from './navegacion';
+import { css } from '@emotion/core';
+
+const Header = () => {
+    return (  
+        <header
+            css = {css`
+                background-color: #0D283B;
+                padding: 1rem;
+            `}
+        >
+            <div
+                css = {css`
+                    max-width: 120rem;
+                    margin: 0 auto;
+                    text-align: center;
+
+                    @media (min-width: 768px){
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-between;
+                    }
+                `}
+            >
+                <Link>
+                    Bienes raices
+                </Link>
+                <Navegacion />
+            </div>
+        </header>
+    );
+}
+ 
+export default Header;
--- bienesraices/bienesraicesFront/src/components/layout.js
+++ bienesraices/bienesraicesFront/src/components/layout.js
@@ -1,6 +1,7 @@
 import React, { Children } from 'react';
 import Helmet from 'react-helmet';
 import { Global, css } from '@emotion/core';
+import Header from './header';
 
 const Layout = ({ children }) => {
     return (  
@@ -52,6 +53,7 @@ const Layout = ({ children }) => {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"></link>
             </Helmet>
+            <Header/>
             <h1>Mi sitio en Gatsby</h1>
             {children}
         </>
