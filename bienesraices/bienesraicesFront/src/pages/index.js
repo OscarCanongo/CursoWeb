d082e375349bdfc07e66ecf03819649539302208
OscarCanongo <a01730443@itesm.mx>
2020-10-30 16:43:13 -0600

    NodeSent

--- bienesraices/bienesraicesFront/src/pages/index.js
+++ bienesraices/bienesraicesFront/src/pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout';
+import useInicio from '../hooks/useInicio';
 
 const Index = () => {
+
+    const inicio = useInicio();
+
     return (  
         <Layout>
             <h1>Index</h1>
