207d373dbb11f9a0b1834dd59e3b75a829bab185
OscarCanongo <a01730443@itesm.mx>
2020-11-25 15:29:43 -0600

    Almacenando los Archivos en el servidor

---
+++ nodeSent/frontend/components/Dropzone.js
@@ -0,0 +1,43 @@
+import React, { useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import clienteAxios from '../config/axios';
+
+const Dropzone = () => {
+
+    const onDrop = useCallback( async (acceptedFiles) => {
+        console.log(acceptedFiles);
+
+        //Crear un form data
+        const formData = new FormData();
+        formData.append('archivo', acceptedFiles[0]);
+
+        const resultado = await clienteAxios.post('/api/archivos', formData);
+
+        console.log(resultado.data);
+    }, []);
+
+    // Extraer contenido de Dropzone
+    const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({ onDrop });
+
+    return (  
+        <div className = "md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
+            <div { ...getRootProps({ className: 'dropzone w-full py-32' }) }>
+                <input
+                    className = "h-100" { ...getInputProps() }
+                />
+                    {
+                        isDragActive ? <p className = "text-2xl text-center text-gray-600">Suelta el archivo</p> :
+                        <div className = "text-center">
+                            <p className = "text-2xl text-center text-gray-600">Selecciona un archivo y arrastralo aquí</p>
+                            <button className = "bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue800" type = "button">
+                                Selecciona archivos para subir
+                            </button>       
+                        </div>
+                    }
+            </div>  
+        </div>
+    );
+}
+ 
+export default Dropzone;
+
