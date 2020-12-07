5e8d7a4e8155e03a7d95227af62c496e57bf5032
OscarCanongo <a01730443@itesm.mx>
2020-12-07 16:27:38 -0600

    testing

--- citas_react_testing/src/__tests__/App.js
+++ citas_react_testing/src/__tests__/App.js
@@ -1,3 +1,71 @@
-test('La aplicacion funciona', () => {
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import useEvent from '@testing-library/user-event';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
 
+test('<App /> La aplicación funciona bien la primera vez', () => {
+    render(<App/>);
+    
+    expect(screen.getByText('Administrador de Pacientes')).toBeInTheDocument();
+    expect(screen.getByTestId('nombre-app').textContent).toBe('Administrador de Pacientes');
+    expect(screen.getByTestId('nombre-app').tagName).toBe('H1');
+    expect(screen.getByText('No hay citas')).toBeInTheDocument();
+    expect(screen.getByText('Crear Cita')).toBeInTheDocument();
+});
+
+test('<App /> Agregar una cita y verificar Header', () => {
+    render(<App/>);
+    
+    useEvent.type(screen.getByTestId('mascota'), 'Hook');
+    useEvent.type(screen.getByTestId('propietario'), 'Oscar');
+    useEvent.type(screen.getByTestId('fecha'), '2020-12-07');
+    useEvent.type(screen.getByTestId('hora'), '15:33');
+    useEvent.type(screen.getByTestId('sintomas'), 'Solo duerme');
+
+    //Click en botón de submit
+    const btnSubmit = screen.getByTestId('btn-submit');
+    useEvent.click(btnSubmit);
+
+    //Revisar por la alerta
+    const alerta = screen.queryByTestId('alerta')
+    expect(alerta).not.toBeInTheDocument();
+
+    //Revisar por el titulo dinamico
+    expect(screen.getByTestId('titulo-dinamico').textContent).toBe('Administra tus Citas');
+    expect(screen.getByTestId('titulo-dinamico').textContent).not.toBe('No hay citas');
+});
+
+test('<App /> Verificar citas en el DOM', async () => {
+    render(<App/>);
+
+    const citas = await screen.findAllByTestId('cita');
+
+    //Snapshot crea un archivo para verificar su contenido
+    //expect(citas).toMatchSnapshot();
+
+    expect(screen.getByTestId('btn-eliminar').tagName).toBe('BUTTON');
+    expect(screen.getByTestId('btn-eliminar')).toBeInTheDocument();
+
+    //Verificar alguna cita
+    expect(screen.getByText('Hook')).toBeInTheDocument();
+});
+
+test('<App /> Elimina la cita', () => {
+    render(<App/>);
+
+    const btnEliminar = screen.getByTestId('btn-eliminar')
+    expect(btnEliminar.tagName).toBe('BUTTON');
+    expect(btnEliminar).toBeInTheDocument();
+
+    //Simular el click
+    userEvent.click(btnEliminar);
+
+    //El boton ya no debe de estar
+    expect(btnEliminar).not.toBeInTheDocument();
+
+    //La cita ya no debe de estar
+    expect(screen.queryByText('Hook')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('cita')).not.toBeInTheDocument();
 });
--- citas_react_testing/src/__tests__/Formulario.js
+++ citas_react_testing/src/__tests__/Formulario.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Formulario from '../components/Formulario';
 import '@testing-library/jest-dom/extend-expect';
+import useEvent from '@testing-library/user-event';
 
 const crearCita = jest.fn();
 
@@ -33,10 +34,37 @@ test('<Formulario /> Validación de formulario', () => {
 
     //Click en botón de submit
     const btnSubmit = screen.getByTestId('btn-submit');
-    fireEvent.click(btnSubmit);
+    useEvent.click(btnSubmit);
 
     //Revisar por la alerta
-    expect(screen.getByTestId('alerta')).toBeInTheDocument();
-    expect(screen.getByTestId('alerta').textContent).toBe('Todos los campos son obligatorios');
-    expect(screen.getByTestId('alerta').tagName).toBe('P');
+    const alerta = screen.getByTestId('alerta')
+    expect(alerta).toBeInTheDocument();
+    expect(alerta.textContent).toBe('Todos los campos son obligatorios');
+    expect(alerta.tagName).toBe('P');
+});
+
+test('<Formulario /> Validación de formulario', () => {
+    render(
+        <Formulario
+            crearCita = {crearCita}
+        />
+    );
+
+    useEvent.type(screen.getByTestId('mascota'), 'Hook');
+    useEvent.type(screen.getByTestId('propietario'), 'Oscar');
+    useEvent.type(screen.getByTestId('fecha'), '2020-12-07');
+    useEvent.type(screen.getByTestId('hora'), '15:33');
+    useEvent.type(screen.getByTestId('sintomas'), 'Solo duerme');
+
+    //Click en botón de submit
+    const btnSubmit = screen.getByTestId('btn-submit');
+    useEvent.click(btnSubmit);
+
+    //Revisar por la alerta
+    const alerta = screen.queryByTestId('alerta')
+    expect(alerta).not.toBeInTheDocument();
+
+    //Crear cita y comprobar que la función se haya llamado
+    expect(crearCita).toHaveBeenCalled();
+    expect(crearCita).toHaveBeenCalledTimes(1);
 });
