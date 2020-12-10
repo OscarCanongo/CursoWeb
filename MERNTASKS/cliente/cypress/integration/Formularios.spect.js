2c61bb2007c43162174aa8126beaae292f5402d6
OscarCanongo <oscarcanongo@outlook.com>
2020-12-09 20:08:05 -0600

    Cypress

---
+++ MERNTASKS/cliente/cypress/integration/Formularios.spect.js
@@ -0,0 +1,43 @@
+/// <reference types = "cypress"/>
+describe( '<Formularios />', () => {
+    it('<Login /> - Verificar pantalla de inicio', () => {
+        cy.visit('/');
+
+        //Probar el texto
+
+        cy.get('[data-cy=titulo]')
+            .invoke('text')
+            .should('equal', 'Iniciar Sesión');
+
+        //Revisar que el formulario exista
+        cy.get('[data-cy=form-login]')
+            .should("exist");
+
+        // Revisar los inputs
+        cy.get('[data-cy=email-input]')
+            .should("exist");
+        
+        cy.get('[data-cy=password-input]')
+            .should("exist");
+    
+        cy.get('[data-cy=submit-login]')
+            .should("exist")
+            .should('have.value', 'Iniciar Sesión')
+            .should('have.class', 'btn-primario')
+            .and('have.class', 'btn');
+        
+        cy.get('[data-cy=nueva-cuenta]')
+            .should("exist")
+            .should('have.prop', 'tagName')
+            .should('eq', 'A')
+
+        cy.get('[data-cy=nueva-cuenta]')
+            .should('have.attr', 'href')
+            .should('eq', '/nueva-cuenta') 
+        
+        cy.visit('/nueva-cuenta');
+    });
+    it('Login /> - Verificar el formulario', () => {
+
+    });
+})
